56d6dc6b35e68f3195eed5b841741ef185cbefd7
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-06-05 17:00:28 -0300

    testa a arvore de busca binaria

--- src/main/java/com/alunosmateuspedro/Main.java
+++ src/main/java/com/alunosmateuspedro/Main.java
@@ -18,13 +18,9 @@ public class Main {
          * System.out.println(tab.remove(238576));
          * System.out.println(tab.busca(238576));
          */
-        int[] myArr = { 442, 532, 2, 35, 6, 2, 3, 665, 4, 6, 458, 6, 54, 7, 8, 4, 35, 346, 90, 4, 457457, 45, 745,
-                48, };
-        ArvoreBinariaBusca<Integer> bTree = new ArvoreBinariaBusca<Integer>();
-        for (int i = 0; i < myArr.length; i++) {
-            bTree.insere(myArr[i]);
-        }
-        Integer elemento = bTree.busca(6);
-        System.out.println(elemento);
+        TabelaHash<Integer,ArvoreBinariaBusca<Integer>> tabela = new TabelaHash<>();
+        tabela.insere(1, new ArvoreBinariaBusca<Integer>());
+        tabela.busca(1).getValor().insere(70);
+        
     }
 }
